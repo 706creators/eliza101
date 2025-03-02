6019d2c89bd177a686909b85d0ffc5e6f11b7072
v1xingyue <qixingyue@gmail.com>
2025-03-03 07:37:53 +0800

    remove old page

--- packages/page/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { type ReactElement } from "react";
-
-export function Loading(): ReactElement {
-  return (
-    <div className="flex items-center justify-center min-h-[200px]">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#FF7A00]"></div>
-    </div>
-  );
-}
--- packages/page/src/hooks/useModules.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '../types/module';
-import { modules } from '../data/modules';
-
-export function useModules(): Module[] {
-  return modules;
-}
