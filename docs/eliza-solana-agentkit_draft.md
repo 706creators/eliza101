c247fa74fb4bea9489557b112338aaea2dd9c6e9
v1xingyue <qixingyue@gmail.com>
2025-03-02 21:13:09 +0800

    update newpage

--- docs/eliza-solana-agentkit_draft.md
+++
@@ -1,6 +0,0 @@
-# Guide to ElizaOS + Solana Agent Kit
-
-
-原文链接: https://medium.com/@0xnitt/guide-to-elizaos-solana-agent-kit-d58ba10b4924
-
-Solana Agent Kit 是很好用的一个 Agent 工具,
---
+++ packages/eliza101/src/app/layout.config.tsx
@@ -0,0 +1,32 @@
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: (
+      <>
+        <svg
+          width="24"
+          height="24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-label="Robot Logo"
+          viewBox="0 0 24 24"
+        >
+          <rect x="4" y="4" width="16" height="16" fill="currentColor" />
+          <rect x="7" y="7" width="3" height="3" fill="#ff0000" />
+          <rect x="14" y="7" width="3" height="3" fill="#00ff00" />
+          <rect x="9" y="14" width="6" height="2" fill="#0000ff" />
+          <rect x="10" y="17" width="4" height="1" fill="#ffff00" />
+        </svg>
+        eliza 101
+      </>
+    ),
+  },
+  links: [],
+};
