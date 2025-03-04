b88b2cc7150a0d64333bab1f41f15b1b76454ba6
v1xingyue <qixingyue@gmail.com>
2025-03-04 10:46:53 +0800

    add youtube link

---
+++ packages/eliza101/src/lib/youtube.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  src: string;
+};
+
+export function Youtube({ src }: Props) {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={src}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+  );
+}
